5004ee22d4c8240792b98712b1851402f733175b
Dimitrios Vlastaras <mail@dimme.net>
2012-01-14 04:12:33 +0100

    Added dependencies

---
+++ Objective-C/MacOSX/BezierCurves/BezierCurves/BezierLayer.h
@@ -0,0 +1,13 @@
+//
+//  BezierLayer.h
+//  BezierCurves
+//
+//  Created by Dimitrios Vlastaras on 1/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CCLayer.h"
+
+@interface BezierLayer : CCLayer
+
+@end
---
+++ Objective-C/MacOSX/BezierCurves/BezierCurves/BezierLayer.m
@@ -0,0 +1,13 @@
+//
+//  BezierLayer.m
+//  BezierCurves
+//
+//  Created by Dimitrios Vlastaras on 1/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BezierLayer.h"
+
+@implementation BezierLayer
+
+@end
